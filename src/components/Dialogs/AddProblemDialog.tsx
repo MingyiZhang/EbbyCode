efc77b2ef5784fc5ed420f6e658a980200d19120
Mingyi Zhang <mingyizhang1987@gmail.com>
2024-01-07 23:34:01 +0100

    Upgraded mui date picker

--- src/components/Dialogs/AddProblemDialog.tsx
+++ src/components/Dialogs/AddProblemDialog.tsx
@@ -85,140 +85,145 @@ const AddProblemDialog = () => {
   };
 
   return (
-    <Dialog
-      fullScreen={fullScreen}
-      open={addProblemDialogOpen}
-      onClose={handleClose}
-      aria-labelledby="add-problem-dialog"
-    >
-      <DialogTitle id="add-problem-dialog-title">{"Add New Problem"}</DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2} justifyContent={"center"} alignItems={"center"} sx={{pt: 2}}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id={"platform"}
-              fullWidth
-              required
-              variant={"outlined"}
-              label={"Platform"}
-              value={platform}
-              onChange={handleChange(setPlatform)}
-              select
-            >
-              <MenuItem value={0}>LeetCode</MenuItem>
-              <MenuItem value={1}>Codility</MenuItem>
-            </TextField>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Autocomplete
-              id="serial"
-              options={leetCodeChoices}
-              getOptionLabel={option => option.id.toString()}
-              onChange={(event, newValue) => {
-                if (newValue !== null) {
-                  handleOnChange(newValue);
-                }
-              }}
-              inputValue={serial}
-              fullWidth
-              renderInput={params =>
-                <TextField
-                  {...params}
+      <Dialog
+          fullScreen={fullScreen}
+          open={addProblemDialogOpen}
+          onClose={handleClose}
+          aria-labelledby="add-problem-dialog"
+      >
+        <DialogTitle id="add-problem-dialog-title">{"Add New Problem"}</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2} justifyContent={"center"} alignItems={"center"} sx={{pt: 2}}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                  id={"platform"}
+                  fullWidth
                   required
                   variant={"outlined"}
-                  label={platform === 0 ? "Serial Number" : "Lesson Number"}
-                />}
-            />
+                  label={"Platform"}
+                  value={platform}
+                  onChange={handleChange(setPlatform)}
+                  select
+              >
+                <MenuItem value={0}>LeetCode</MenuItem>
+                <MenuItem value={1}>Codility</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Autocomplete
+                  id="serial"
+                  options={leetCodeChoices}
+                  getOptionLabel={option => option.id.toString()}
+                  onChange={(event, newValue) => {
+                    if (newValue !== null) {
+                      handleOnChange(newValue);
+                    }
+                  }}
+                  inputValue={serial}
+                  fullWidth
+                  renderInput={params =>
+                      <TextField
+                          {...params}
+                          required
+                          variant={"outlined"}
+                          label={platform === 0 ? "Serial Number" : "Lesson Number"}
+                      />}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Autocomplete
+                  id="title"
+                  options={leetCodeChoices}
+                  getOptionLabel={option => option.title}
+                  onChange={(event, newValue) => {
+                    if (newValue !== null) {
+                      handleOnChange(newValue);
+                    }
+                  }}
+                  inputValue={title}
+                  fullWidth
+                  renderInput={params =>
+                      <TextField
+                          {...params}
+                          required
+                          variant="outlined"
+                          label="Problem Title"
+                      />}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {
+                  platform === 0 &&
+                  <TextField
+                      disabled={leetCodeChoices.length !== 0}
+                      id={"difficulty"}
+                      fullWidth
+                      required
+                      variant={"outlined"}
+                      label={"Difficulty"}
+                      value={difficulty}
+                      onChange={handleChange(setDifficulty)}
+                      select
+                  >
+                    <MenuItem value={0}>Easy</MenuItem>
+                    <MenuItem value={1}>Medium</MenuItem>
+                    <MenuItem value={2}>Hard</MenuItem>
+                  </TextField>
+              }
+              {
+                  platform === 1 &&
+                  <TextField
+                      id={"difficulty"}
+                      fullWidth
+                      required
+                      variant={"outlined"}
+                      label={"Difficulty"}
+                      value={difficulty}
+                      onChange={handleChange(setDifficulty)}
+                      select
+                  >
+                    <MenuItem value={4}>Painless</MenuItem>
+                    <MenuItem value={5}>Respectable</MenuItem>
+                  </TextField>
+              }
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                    slotProps={{textField: {color: 'secondary'}}}
+                    ampm={false}
+                    value={time}
+                    label="Create Time"
+                    onChange={(date) => setTime(date)}
+                    onError={console.log}
+                    viewRenderers={{
+                      hours: null,
+                      minutes: null,
+                      seconds: null,
+                    }}
+                />
+              </LocalizationProvider>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Autocomplete
-              id="title"
-              options={leetCodeChoices}
-              getOptionLabel={option => option.title}
-              onChange={(event, newValue) => {
-                if (newValue !== null) {
-                  handleOnChange(newValue);
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button
+              onClick={() => {
+                if (serial && title) {
+                  handleAdd();
+                  handleClose();
                 }
               }}
-              inputValue={title}
-              fullWidth
-              renderInput={params =>
-                <TextField
-                  {...params}
-                  required
-                  variant="outlined"
-                  label="Problem Title"
-                />}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            {
-              platform === 0 &&
-              <TextField
-                disabled={leetCodeChoices.length !== 0}
-                id={"difficulty"}
-                fullWidth
-                required
-                variant={"outlined"}
-                label={"Difficulty"}
-                value={difficulty}
-                onChange={handleChange(setDifficulty)}
-                select
-              >
-                <MenuItem value={0}>Easy</MenuItem>
-                <MenuItem value={1}>Medium</MenuItem>
-                <MenuItem value={2}>Hard</MenuItem>
-              </TextField>
-            }
-            {
-              platform === 1 &&
-              <TextField
-                id={"difficulty"}
-                fullWidth
-                required
-                variant={"outlined"}
-                label={"Difficulty"}
-                value={difficulty}
-                onChange={handleChange(setDifficulty)}
-                select
-              >
-                <MenuItem value={4}>Painless</MenuItem>
-                <MenuItem value={5}>Respectable</MenuItem>
-              </TextField>
-            }
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DateTimePicker
-                renderInput={(props) => <TextField {...props} fullWidth color="secondary"/>}
-                ampm={false}
-                value={time}
-                label="Create Time"
-                onChange={(date) => setTime(date)}
-                onError={console.log}
-              />
-            </LocalizationProvider>
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button autoFocus onClick={handleClose} color="secondary">
-          Cancel
-        </Button>
-        <Button
-          onClick={() => {
-            if (serial && title) {
-              handleAdd();
-              handleClose();
-            }
-          }}
-          color="primary"
-          autoFocus
-        >
-          OK
-        </Button>
-      </DialogActions>
-    </Dialog>
+              color="primary"
+              autoFocus
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
   );
 };
 
